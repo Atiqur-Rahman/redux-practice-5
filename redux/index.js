42d473583020a3aae18cc56d8de5bfb6b7459da4
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-11-14 18:28:50 +0600

    redux counter is completed

---
+++ redux/index.js
@@ -0,0 +1,14 @@
+const { increment, decrement } = require('./counter/actions');
+const store = require('./store');
+
+// subscribe to state change
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+// dispatch action
+store.dispatch(increment());
+
+store.dispatch(increment());
+
+store.dispatch(decrement());
